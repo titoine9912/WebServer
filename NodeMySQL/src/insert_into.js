77755c090a5057b957d827945efe564d510ee010
Daesmoon <Kasandra_b@hotmail.fr>
2018-10-18 09:57:26 -0400

    insert

--- NodeMySQL/src/insert_into.js
+++ NodeMySQL/src/insert_into.js
@@ -1,13 +1,12 @@
-var mysql = require('mysql');
+import Database from "better-sqlite3";
 
-var con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "nodebd"
-});
-
-con.connect(function(err) {
+//simple
+function insertInto() {
+	let db = new Database(dbFilePath);
+	db.exec("INSERT INTO score(name, score) VALUES ('Name', 9000);");
+	db.close(); 
+}
+/*con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
   var sql = "INSERT INTO score (name, score) VALUES ('Antoine', 1000)";
@@ -15,4 +14,36 @@ con.connect(function(err) {
     if (err) throw err;
     console.log("1 record inserted");
   });
-});
+});*/
+
+//multiple
+function insertMultiple() {
+	let db = new Database(dbFilePath);
+	let values = [
+		['John', 1000],
+		['Antoine', 325],
+		['Amy', 652],
+		['Hannah', 2000],
+		['Michael', 3000]
+	];
+	db.exec("INSERT INTO score (name, score) VALUES ?;");
+	db.close(); 
+}
+/*con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+  var sql = "INSERT INTO score (name, score) VALUES ?";
+  var values = [
+    ['John', 1000],
+    ['Antoine', 325],
+    ['Amy', 652],
+    ['Hannah', 2000],
+    ['Michael', 3000]
+  ];
+  con.query(sql, [values], function (err, result) {
+    if (err) throw err;
+    console.log("Number of records inserted: " + result.affectedRows);
+  });
+});*/
+
+export {insertInto as default, (insertMultiple)};
